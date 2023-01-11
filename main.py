5564b5ca81927cd8f21d61e1e40cbd583d14d13b
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2023-01-10 18:09:48 -0600

    working out matching with regex for dif starts

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+
 practice_file = """ doe: "a deer, a female deer"
  ray: "a drop of golden sun"
  pi: 3.14159
@@ -22,18 +24,25 @@ def yaml_to_dict(file: str):
     raw = file.splitlines()
     # print(raw)
     for line in raw:
-        split_line = line.split(':')
+        split_line = line.split(":")
         if len(split_line) > 1:
             key, value = split_line[0], split_line[1]
         else:
             print(line)
 
-        print(key)
-        print(value)
+        # print(key)
+        # print(value)
 
 
-if __name__ == '__main__':
-    yaml_to_dict(practice_file)
+if __name__ == "__main__":
+    # yaml_to_dict(practice_file)
+    #  \- *\\w+/g
+    LIST_PATTERN_STR = r" \- *\w+"
+    LIST_PATTERN_LEN = 3
+    pattern = re.compile(LIST_PATTERN_STR)
+    matches = pattern.finditer(practice_file)
+    for match in matches:
+        print(practice_file[match.start() + LIST_PATTERN_LEN : match.end()])
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
